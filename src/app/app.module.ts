1076f626421425ab58e701f4e6249b676612c20b
Fabian Trampusch <fabian.trampusch@freenet.de>
2020-03-10 16:40:00 +0100

    initial testing commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgxChatModule} from '../../projects/pazznetwork/ngx-chat/src/lib/ngx-chat.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        NgxChatModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
